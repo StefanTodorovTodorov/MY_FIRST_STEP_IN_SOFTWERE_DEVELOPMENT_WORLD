bd9f6f76a96320e12f084a6966d04dd158346900
Stefan Todorov <stefoviva@abv.bg>
2018-01-05 09:42:04 +0200

    Added RectangleArea.cs
    It input 2 real numbers calculate rectangle area and print area result.

---
+++ FirstStepsInCoding/RectangleArea.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class RectangleArea
+{
+    static void Main()
+    {
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        double c = a * b;
+        Console.WriteLine(c);
+    }
+}
