f35581b643350e0fc1f551a6a68662a09bfc1058
Stefan Todorov <stefoviva@abv.bg>
2018-01-05 09:37:19 +0200

    Added TriangleOf55Stars.cs
    It draw triangle of stars

---
+++ FirstStepsInCoding/TriangleOf55Stars.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class TriangleOf55Stars
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            for (int j = 1; j <= i; j++)
+                Console.Write("*");
+                Console.WriteLine();
+
+        }
+    }
+}
