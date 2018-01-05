f52a69ec8c39e4c9c8fb78726d9b4ab1d3497f0d
Stefan Todorov <stefoviva@abv.bg>
2018-01-05 09:15:08 +0200

    Added Hello SoftUni
    It is printing greeting "Hello SoftUni"

---
+++ FirstStepsInCoding/HelloSoftUni.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class FirstProj
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello SoftUni");
+    }
+}
